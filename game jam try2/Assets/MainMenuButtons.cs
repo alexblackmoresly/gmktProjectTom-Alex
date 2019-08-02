e7c58aa7f73faa4133649faae40846927ac01d53
alexblackmoresly <alexblackmoresly@users.noreply.github.com>
2019-08-02 22:57:35 +0100

    inital

---
+++ game jam try2/Assets/MainMenuButtons.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public int levelChosen;
+    public void getLevelChosen(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+    // Start is called before the first frame update
+    public void PlayLevel1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    void Start()
+    {
+        
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
